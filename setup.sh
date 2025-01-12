a2dfcb5ee63b8d7dce039c959a8c23d10182f76c
damon <sycamoremoon376@gmail.com>
2025-01-12 23:19:28 +0800

    Fix bug and add mailbox dotfile

--- setup.sh
+++ setup.sh
@@ -8,12 +8,12 @@ dotsetup() {
 	local actual_file="$HOME/.dotfile/$file"
 	local linked_file="$HOME/.$file"
 
-	if [ ! -f "$actual_file" ]; then
+	if [ ! -e "$actual_file" ]; then
 		echo "$actual_file is not existed!"
-		return 1
+		exit 1
 	fi
 
-	if [ -f "$linked_file" ]; then
+	if [ -e "$linked_file" ]; then
 		diff "$actual_file" "$linked_file"
 		if [[ "$?" != 0 ]]; then
 			ln -s $FORCE $actual_file $linked_file
@@ -23,8 +23,18 @@ dotsetup() {
 			exit 1
 		fi
 	else
+		echo "$linked_file no exist, create new"
 		ln -s $FORCE $actual_file $linked_file
 	fi
+}
+
+setupmailbox() {
+	local account=$1
+	local force=$2
+	ln -s$force $HOME/.local/share/mail/$account/{\[Gmail\]/Drafts,Drafts}
+	ln -s$force $HOME/.local/share/mail/$account/{\[Gmail\]/Spam,Junk}
+	ln -s$force $HOME/.local/share/mail/$account/{\[Gmail\]/Sent\ Mail,Sent}
+	ln -s$force $HOME/.local/share/mail/$account/{\[Gmail\]/Trash,Trash}
 }
 
 # git
@@ -32,12 +42,20 @@ dotsetup gitconfig
 
 # shell
 dotsetup zshrc
-# dotsetup sh_alias
+dotsetup sh_alias
 
 # tmux
-# dotsetup tmux.conf
+dotsetup tmux.conf
 
 # vim
-# dotsetup vimrc
-# dotsetup vim
+dotsetup vimrc
+dotsetup vim
 
+# ssh
+dotsetup ssh/config
+
+# mail
+# setupmailbox sycamoremoon376@gmail.com
+dotsetup config/mutt/accounts/sycamoremoon376@gmail.com.muttrc
+dotsetup local/share/mutt-wizard/mutt-wizard.muttrc
+dotsetup config/msmtp/config
